6d6ad22bb614fbb3df99df667ffa8f61a39a4e63
Ankita-Nimkar <ankita.dandwate.nimkar@gmail.com>
2025-07-13 20:16:06 +0530

    responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+
 const App = () => {
-  return <Button>Click me</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        md: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="blue.500">
+        Nav
+      </GridItem>
+
+      <GridItem
+        area="aside"
+        bg="green.500"
+        display={{ base: "none", md: "block" }}
+      >
+        Aside
+      </GridItem>
+
+      <GridItem area="main" bg="pink.500">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
